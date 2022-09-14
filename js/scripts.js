549778e3ea605042d19d397c28d4aa16dd5f799d
Rubel Hossain99 <rubel820746@gmail.com>
2022-09-14 15:36:10 +0600

    Search Field Dynamic Done

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 
 // Load Data from api
-const loadPhoneData = async() => {
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+const loadPhoneData = async(searchFieldValue) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchFieldValue}`;
     const res = await fetch(url);
     const dataPhone = await res.json();
     getThephoneData(dataPhone.data);
@@ -9,8 +9,8 @@ const loadPhoneData = async() => {
 
 const getThephoneData = phones =>{
     const phoneID = document.getElementById('phones-all');
+    phoneID.innerText = '';
     phones.forEach(phone => {
-        console.log(phone.image);
         const createDiv = document.createElement('div');
         createDiv.classList.add('col');
         createDiv.innerHTML = `
@@ -21,11 +21,24 @@ const getThephoneData = phones =>{
              <h5 class="card-title">${phone.phone_name}</h5>
              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
              </div>
+            <div class="container mb-3">
+              <div class="row">
+                <div class="col-md-6">Brand Name: <b>${phone.brand}</b></div>
+                <div class="col-md-6"><button type="button" class="btn btn-info">See Details</button></div>
+              </div>
+            </div>
          </div>
          </div>
         `;
         phoneID.appendChild(createDiv);
     });
 }
+
+// Search Button 
+document.getElementById('search-btn').addEventListener('click', function(){
+  const searchField = document.getElementById('search-field');
+  const searchFieldValue =searchField.value;
+  loadPhoneData(searchFieldValue);
+});
 
 loadPhoneData()
